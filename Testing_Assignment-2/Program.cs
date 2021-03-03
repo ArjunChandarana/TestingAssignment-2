7774678dc34afa3319560cc753ca08b816864f99
Arjun Chandarana <achandarana801@gmail.com>
2021-03-03 11:38:32 +0530

    Add project files.

---
+++ Testing_Assignment-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string");
+            string inputString = Console.ReadLine();
+            string output;
+            Console.WriteLine("Enter operation");
+            string operation = Console.ReadLine();
+
+            
+            output = ExtensionMethods.StringConvert(inputString, operation);
+            Console.WriteLine(output);
+
+          
+            Console.WriteLine("Press Enter to Exit");
+            inputString = Console.ReadLine();
+        }
+    }
+}
